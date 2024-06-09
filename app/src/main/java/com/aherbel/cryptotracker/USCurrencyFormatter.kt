0d3a43a72cacf8c92ae80184c8c6fb40c507377b
augustoherbel <augustoherbel.it@gmail.com>
2024-06-09 16:51:32 -0300

    Refactor USCurrencyFormatter to use NumberFormat from java.text package that allow to bring a currency formatter with a specified Locale

--- app/src/main/java/com/aherbel/cryptotracker/USCurrencyFormatter.kt
+++ app/src/main/java/com/aherbel/cryptotracker/USCurrencyFormatter.kt
@@ -1,15 +1,14 @@
 package com.aherbel.cryptotracker
 
 import com.aherbel.cryptotracker.domain.CurrencyFormatter
-import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.Locale
 
 class USCurrencyFormatter : CurrencyFormatter {
     override fun format(amount: Double): String {
-        return DecimalFormat().apply {
+        return NumberFormat.getCurrencyInstance(Locale.US).apply {
             maximumFractionDigits = 2
             minimumFractionDigits = 0
-            negativePrefix = "$-"
-            positivePrefix = "$"
         }.format(amount)
     }
 }
