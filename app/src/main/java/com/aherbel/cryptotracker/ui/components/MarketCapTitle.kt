c58dcc04bf9796bfd4e00d1d6590cb35a3b3f389
augustoherbel <augustoherbel.it@gmail.com>
2024-06-11 23:54:24 -0300

    Replace Text for Title in MarketCapTitle

--- app/src/main/java/com/aherbel/cryptotracker/ui/components/MarketCapTitle.kt
+++ app/src/main/java/com/aherbel/cryptotracker/ui/components/MarketCapTitle.kt
@@ -1,24 +1,17 @@
 package com.aherbel.cryptotracker.ui.components
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import com.aherbel.cryptotracker.R
 
 @Composable
 fun MarketCapTitle() {
-    Text(
-        text = stringResource(id = R.string.market_cap),
-        color = Color.Black,
-        fontSize = 13.sp,
-        fontWeight = FontWeight.Bold,
+    Title(
+        title = stringResource(id = R.string.market_cap),
         modifier = Modifier.semantics { contentDescription = "MarketCapTitle" }
     )
 }
