0e825692e34a4ccfb40fb0d716cec2f6b32ab4fd
augustoherbel <augustoherbel.it@gmail.com>
2024-06-13 21:45:10 -0300

    Refactor assertion

--- app/src/test/java/com/aherbel/cryptotracker/HomeViewModelTest.kt
+++ app/src/test/java/com/aherbel/cryptotracker/HomeViewModelTest.kt
@@ -11,7 +11,6 @@ import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.present
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
-import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 
@@ -38,18 +37,19 @@ class HomeViewModelTest {
     @Test
     fun `test marketCapVariation get formatted with 2 decimal`() = runTest {
         fakeMarketDataRepository.items = listOf(
-            aMarketData().withMarketCapVariation(10.1234).build()
+            aMarketData()
+                .withMarketCapVariation(10.1234)
+                .build()
         )
 
         mainViewModel.requestMarketData()
 
         val mainUiState = mainViewModel.uiState.first()
         val marketCapVariation = mainUiState.marketDataUi.marketCapVariation
-        val expected = "10.12%"
-        assertEquals(
-            "MarketCapVariation was formatted incorrectly.",
-            expected,
-            marketCapVariation
+
+        assertThat(
+            marketCapVariation,
+            equalTo("10.12%")
         )
     }
 }
