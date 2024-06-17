e425c21b6c848a399a02359d289b68625ae00e4e
augustoherbel <augustoherbel.it@gmail.com>
2024-06-17 20:51:00 -0300

    Rename tests of DecimalPercentageFormatterTest

--- app/src/test/java/com/aherbel/cryptotracker/application/DecimalPercentageFormatterTest.kt
+++ app/src/test/java/com/aherbel/cryptotracker/application/DecimalPercentageFormatterTest.kt
@@ -1,7 +1,8 @@
 package com.aherbel.cryptotracker.application
 
-import junit.framework.TestCase.assertEquals
 import org.junit.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
 
 class DecimalPercentageFormatterTest {
 
@@ -9,48 +10,46 @@ class DecimalPercentageFormatterTest {
 
     private fun testResults(
         expected: String,
-        actual: String
+        actual: Double
     ) {
-        assertEquals(
-            "Value got formatted with wrong format. Expected: $expected, got: $actual",
-            expected,
-            actual
-        )
+        expectThat(formatter.format(actual))
+            .describedAs(actual.toString())
+            .isEqualTo(expected)
     }
 
     @Test
     fun `test value get formatted with 2 decimal places`() {
-        testResults("10.12%", formatter.format(10.12))
+        testResults("10.12%", 10.12)
     }
 
     @Test
     fun `test value get formatted with 0 decimal places when there are no decimals`() {
-        testResults("10%", formatter.format(10.0))
+        testResults("10%", 10.0)
     }
 
     @Test
-    fun `test value get formatted with 1 decimal place when there is only 1 decimal`() {
-        testResults("10.1%", formatter.format(10.1))
+    fun `value get formatted with 1 decimal place when there is only 1 decimal`() {
+        testResults("10.1%", 10.1)
     }
 
     @Test
-    fun `test value get formatted with a max of 2 decimal places when there is more than 2 decimals`() {
-        testResults("10.11%", formatter.format(10.1122))
+    fun `value get formatted with a max of 2 decimal places when there is more than 2 decimals`() {
+        testResults("10.11%", 10.1122)
     }
 
     @Test
-    fun `test value get rounded up when formatted`() {
-        testResults("10.12%", formatter.format(10.1162))
+    fun `value get rounded up when formatted`() {
+        testResults("10.12%", 10.1162)
     }
 
     @Test
-    fun setMinPrecisionDigits_afterFormatting_formatsValueRespectingMinimumFractionDigits() {
+    fun `set minPrecisionDigits to 2 formats value with 2 decimal places`() {
         formatter.setMinPrecisionDigits(2)
-        testResults("10.12%", formatter.format(10.1162))
+        testResults("10.12%", 10.1162)
     }
 
     @Test(expected = IllegalArgumentException::class)
-    fun setMinPrecisionDigits_whenConfigured_throwErrorIfDigitsAreNegative() {
+    fun `set minPrecisionDigits throw error when setting negative digits`() {
         formatter.setMinPrecisionDigits(-1)
     }
 }
